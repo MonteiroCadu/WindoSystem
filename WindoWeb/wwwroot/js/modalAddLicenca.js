f58f5292ff13ab1229edd3ae15b88af554b01d45
MonteiroCadu <cadumonteiro.setmidia@gmail.com>
2022-04-06 16:14:26 -0300

    Refactoring da View Detalhe pessoa,  separando o JS em um arquivo de js e separando o codigo do modal de add Licenca em uma partial view

---
+++ WindoWeb/wwwroot/js/modalAddLicenca.js
@@ -0,0 +1,114 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function FillPlataformaSelect() {
+
+    $.get("/Plataforma/List/", function (data) {
+        if (data != null && data.length > 0) {
+
+            $('#plataforma').empty();
+            $('#planoVendas').empty();
+            $('#vencimento').val('');
+
+            $('#plataforma').append('<option value="-1">Selecione</option>');
+
+            data.forEach(function (plataforma, i) {
+                $('#plataforma').append('<option value="' + plataforma.id + '"> ' + plataforma.nome + ' </option>');
+            });
+        }
+    });
+
+    $.get("/Corretora/List/", function (data) {
+        if (data != null && data.length > 0) {
+
+            $('#corretora').empty();
+
+            $('#corretora').append('<option value="-1">Selecione</option>');
+
+            data.forEach(function (corretora, i) {
+                $('#corretora').append('<option value="' + corretora.id + '"> ' + corretora.nome + ' </option>');
+            });
+        }
+    });
+
+};
+
+
+function ChengePlataforma() {
+
+    var plataforma = $('#plataforma').val();
+    $('#planoVendas').empty();
+    $('#vencimento').val('');
+
+    $.get("/Plataforma/GetPlanos/" + plataforma, function (data) {
+        if (data != null && data.length > 0) {
+
+            $('#planoVendas').empty();
+            $('#planoVendas').append('<option value="-1">Selecione</option>');
+
+            data.forEach(function (plano, i) {
+                $('#planoVendas').append('<option value="' + plano.id + '"> ' + plano.nomeComValor + ' </option>');
+            });
+        }
+    });
+
+};
+
+function ChengePlano() {
+
+    var plano = $('#planoVendas').val();
+    $('#vencimento').val('');
+
+    $.get("/PlanoVenda/Get/" + plano, function (data) {
+        if (data != null) {
+
+            $('#vencimento').val(data.vencimento);
+
+        }
+    });
+
+};
+
+function AddLicenca() {
+    var idPessoa = $('#id').val();
+    var plataforma = $('#plataforma').val();
+    var plano = $('#planoVendas').val();
+    var contaCorretora = $('#contaCorretora').val();
+
+    var divErroModal = $('#divErroModal');
+    if (!this.ValidateModal(idPessoa, plataforma, plano, contaCorretora)) {
+
+        divErroModal.css("display", "block");
+
+    } else {
+        divErroModal.css("display", "none");
+    }
+
+    var licenca = {
+        'pessoa': idPessoa,
+        'plataforma': plataforma,
+        'planoVenda': plano,
+        'contaCorretora': contaCorretora
+    }
+
+    $.post("/Licenca/AddToCliente", licenca, function (data) { });
+
+
+};
+
+function ValidateModal(idPessoa, plataforma, plano, contaCorretora) {
+    var idPessoa = $('#id').val();
+    var plataforma = $('#plataforma').val();
+    var plano = $('#planoVendas').val();
+    var contaCorretora = $('#contaCorretora').val();
+    var corretora = $('#contaCorretora').val();
+
+    if (plataforma == null || plataforma === "-1") return false;
+    if (plano == null || plano === "-1") return false;
+    if (corretora == null || corretora === "-1") return false;
+    if (contaCorretora == null || contaCorretora === "") return false;
+
+    return true;
+
+}
